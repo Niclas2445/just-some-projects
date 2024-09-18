3b2865553e207c6db365c0f63f4f423aa1edf46b
Niclas Holmquist <nicl2445@edu.ucl.dk>
2024-09-18 10:47:24 +0200

    first

---
+++ Puzzle1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+for (int x=0; x <= 9; x++)
+{
+    string i = "";
+    for (int j=0; j <= 9; j++)
+    {
+        var bla = x;
+        var bla2 = j;
+        if (x==j || x+j==9)
+        {
+            i += "[x]";
+        }
+        else
+        {
+            i += "[0]";
+        }
+    }
+    Console.WriteLine(i);
+};
+
+string? v = Console.ReadLine();
---
+++ Puzzle3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+Console.WriteLine("Max number: ");
+int max = int.Parse(Console.ReadLine());
+Console.WriteLine("Check multiplyers for");
+Console.WriteLine("1:");
+int multiplyer1 = int.Parse(Console.ReadLine());
+Console.WriteLine("2:");
+int multiplyer2 = int.Parse(Console.ReadLine());
+for (int x=1; x < max; x++)
+{
+    if (x%multiplyer1==0 && x%multiplyer2==0)
+    {
+        Console.WriteLine(x);
+    }
+}
